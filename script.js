1517a723b3709931778dfe667c99d3116e3026b9
Marcos Bernasconi <xb3rna@gmail.com>
2023-03-13 10:53:26 +0100

    All files

---
+++ script.js
@@ -0,0 +1,58 @@
+ 
+ 
+let startAnimation = gsap.timeline({defaults:{opacity:0,ease: "power3.out",duration:1.5}});
+
+startAnimation.from(".hero h1",{
+x: -10, 
+
+})
+
+.from(".hero h3",{
+x: -10, 
+
+},"<+=0.5")
+
+
+.from(".hero button",{
+    y: 10, 
+},"<=0.5")
+
+.from("video",{
+    duration: 2,  
+  },"<+=1")
+
+
+
+let mm = gsap.matchMedia();
+
+mm.add("(min-width:1300px)",()=>{
+
+
+    let animation = gsap.timeline();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // animation.from("video",{ 
+    //   yPercent:-100,duration: 2, ease: "power3.out"});
+    
+    //   animation.pause();
+    
+    // let video = document.querySelector("video");
+    
+    // video.oncanplay = ()=>{animation.resume();console.log("Animation")};
+ 
+    // return () => { 
+    //   };
+});
